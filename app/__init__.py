20e225ed35c1a3966ea73ee1ea56d07d7352165d
dxxpxy <deep2.dhirajlal@live.uwe.ac.uk>
2023-02-19 17:15:44 +0000

    add

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, render_template, request, flash, redirect
 #import sqlite3
 from datetime import datetime
-#from db_func import *
+from db_func import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
@@ -11,30 +11,30 @@ def home():
     return render_template("home.html")
 
 
-# @app.route('/addinvoice', methods = ['GET', 'POST'])
-# def addinvoice(): 
+@app.route('/addinvoice', methods = ['GET', 'POST'])
+def addinvoice(): 
          
-#     if request.method == 'POST':
+    if request.method == 'POST':
         
         
-#         customername = request.form.get('customername')
-#         customeraddress = request.form.get('customeraddress')
-#         date = request.form.get('date')       
-#         description = request.form.get('desp')
-#         invoiceno = request.form.get('invoiceno')
-#         invoicetotal = request.form.get('invoicetotal')
-#         #flash('Invoice added!', category='greenlight')
-#         error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
-#         if error is not None:
-#             flash(error, category='redlight')
-#         else:
-#             #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
-#             #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
-#             #db_func.check_db_exist()
-#             db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
-#             flash('Invoice added!', category='greenlight')
+        customername = request.form.get('customername')
+        customeraddress = request.form.get('customeraddress')
+        date = request.form.get('date')       
+        description = request.form.get('desp')
+        invoiceno = request.form.get('invoiceno')
+        invoicetotal = request.form.get('invoicetotal')
+        #flash('Invoice added!', category='greenlight')
+        error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
+        if error is not None:
+            flash(error, category='redlight')
+        else:
+            #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
+            #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
+            #db_func.check_db_exist()
+            db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
+            flash('Invoice added!', category='greenlight')
           
-#     return render_template("Form.html")
+    return render_template("Form.html")
 
 
 # @app.route('/viewinvoice')
