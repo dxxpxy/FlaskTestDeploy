812cb235bd6da0fc011ab4167a4e1d9d259bfb37
dxxpxy <deep2.dhirajlal@live.uwe.ac.uk>
2023-02-19 18:05:36 +0000

    sdf

--- app/__init__.py
+++ app/__init__.py
@@ -15,14 +15,14 @@ def home():
 @app.route('/addinvoice', methods = ['GET', 'POST'])
 def addinvoice(): 
          
-    if request.method == 'POST':
+    # if request.method == 'POST':
                 
-        customername = request.form.get('customername')
-        customeraddress = request.form.get('customeraddress')
-        date = request.form.get('date')       
-        description = request.form.get('desp')
-        invoiceno = request.form.get('invoiceno')
-        invoicetotal = request.form.get('invoicetotal')
+    #     customername = request.form.get('customername')
+    #     customeraddress = request.form.get('customeraddress')
+    #     date = request.form.get('date')       
+    #     description = request.form.get('desp')
+    #     invoiceno = request.form.get('invoiceno')
+    #     invoicetotal = request.form.get('invoicetotal')
         #flash('Invoice added!', category='greenlight')
         
         # error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
