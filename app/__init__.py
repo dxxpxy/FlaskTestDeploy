1e596413fbb003d9e3ebf3bdaf09d1d5793cb4a2
dxxpxy <deep2.dhirajlal@live.uwe.ac.uk>
2023-02-19 18:57:13 +0000

    ugh

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,8 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/addinvoice', methods = ['GET', 'POST'])
-def addinvoice(): 
+# @app.route('/addinvoice', methods = ['GET', 'POST'])
+# def addinvoice(): 
          
     # if request.method == 'POST':
                 
@@ -23,19 +23,19 @@ def addinvoice():
     #     description = request.form.get('desp')
     #     invoiceno = request.form.get('invoiceno')
     #     invoicetotal = request.form.get('invoicetotal')
-        #flash('Invoice added!', category='greenlight')
+    #     flash('Invoice added!', category='greenlight')
         
-        # error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
-        # if error is not None:
-        #     flash(error, category='redlight')
-        # else:
-        #     #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
-        #     #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
-        #     #db_func.check_db_exist()
-        #     db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
-        #     flash('Invoice added!', category='greenlight')
+    #     error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
+    #     if error is not None:
+    #         flash(error, category='redlight')
+    #     else:
+    #         #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
+    #         #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
+    #         #db_func.check_db_exist()
+    #         db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
+    #         flash('Invoice added!', category='greenlight')
           
-    return render_template("Form.html")
+    # return render_template("Form.html")
 
 
 # @app.route('/viewinvoice')
